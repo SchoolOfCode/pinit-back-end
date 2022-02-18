7912d4f9fa877a6f5b25c44d7bcb8f082209c701
Macsen Sully-Morgan <macsullymorgan@gmail.com>
2022-02-18 15:08:55 +0000

    routes written, still in need of testing once functions are linked

--- routes/media.js
+++ routes/media.js
@@ -1,4 +1,5 @@
 import express from "express";
+import res from "express/lib/response";
 import { getAllMedia, addMedia } from "../models/media.js";
 
 const router = express.Router();
@@ -30,12 +31,13 @@ router.get("/media", async function (req, res) {
    if (data) {
       return res.json({
          success: true,
+         message:"Fetched all media",
          payload: data,
       });
    } else {
       return res.json({
          success: false,
-         payload: "something went wrong",
+         message: "something went wrong",
       });
    }
 });
@@ -43,16 +45,18 @@ router.get("/media", async function (req, res) {
 // GET media by id
 router.get("/media/:id", async function (req, res) {
    //can't call the function until created/imported
-   const data = await getMediaById(req.params.id);
+   const id = req.params.id;
+   const data = await getMediaById(id);
    if (data) {
       return res.json({
          success: true,
+         message:`Fetched media with the id ${id}`,
          payload: data,
       });
    } else {
       return res.json({
          success: false,
-         payload: "something went wrong",
+         message: "something went wrong",
       });
    }
 });
@@ -60,16 +64,18 @@ router.get("/media/:id", async function (req, res) {
 // GET media by location
 router.get("/media/location/:location", async function (req, res) {
    //can't call the function until created/imported
-   const data = await getMediaByLocation(req.params.location); // TODO: check if :location is correct param
+   const location = req.params.location;
+   const data = await getMediaByLocation(location); // TODO: check if :location is correct param
    if (data) {
       return res.json({
          success: true,
+         message:`Media fetched from the location ${location}`,
          payload: data,
       });
    } else {
       return res.json({
          success: false,
-         payload: "something went wrong",
+         message: "something went wrong",
       });
    }
 });
@@ -77,18 +83,107 @@ router.get("/media/location/:location", async function (req, res) {
 // GET media by date
 router.get("/media/date/:date", async function (req, res) {
    //can't call the function until created/imported
-   const data = await getMediaByDate(req.params.date); // TODO: check if :date is correct param
+   const date = req.params.date;
+   const data = await getMediaByDate(date); // TODO: check if :date is correct param
    if (data) {
       return res.json({
          success: true,
+         message:`Media fetched with the date ${date}`,
          payload: data,
       });
    } else {
       return res.json({
          success: false,
-         payload: "something went wrong",
+         message: "something went wrong",
       });
    }
 });
+
+//POST media
+router.post("/media", async function(req, res) {
+  const data = req.body;
+  const result = await FUNCTION(data) ;
+  if (!result) {
+   res.json({
+     success: false,
+     message:"something broke, we couldn't post the media."
+   }) 
+  }
+  res.json({
+    success: true,
+    message:"Image posted to the app",
+    payload: result,
+  })
+})
+
+//DELETE media by id 
+router.delete("/media/:id", async function (req, res) {
+  const id = req.params.id;
+  const result = await FUNCTION(id);
+  if(!result) {
+    res.json ({
+      success: false,
+      message:`something broke, we couldn't find ${id}`,
+    })
+  } else {
+    res.json({
+      success:true,
+      message:`Deleted media with the id ${id}`,
+      payload: result,
+    })
+  }
+})
+//DELETE all media by location
+router.delete("/media/location/:location", async function (req, res) {
+  const location = req.params.location;
+  const result = await FUNCTION(location);
+  if(!result) {
+    res.json ({
+      success: false,
+      message:`something went brokey wokey, we couldn't find ${location}`,
+    })
+  } else {
+    res.json({
+      success:true,
+      message:`Deleted all media at the location ${location}`,
+      payload: result,
+    })
+  }
+})
+//DELETE all media by date
+router.delete("/media/date/:date", async function (req, res) {
+  const date = req.params.date;
+  const result = await FUNCTION(date);
+  if(!result) {
+    res.json ({
+      success: false,
+      message:`something broke, we couldn't find ${date}`,
+    })
+  } else {
+    res.json({
+      success:true,
+      message:`Deleted all media on the date ${date}`,
+      payload: result,
+    })
+  }
+})
+//PUT media by id
+router.put("/media/:id", async function(req, res) {
+  const id = req.params.id;
+  const update = req.body;
+  const result = await FUNCTION(update, id);
+  if (!result) {
+    res,json ({
+      success: false,
+      message: "something went oops, sorry",
+    })
+  } else {
+    res.json ({
+      success: true,
+      message:"image post updated",
+      payload: result,
+    })
+  }
+})
 
 export default router;
