1dbbd102761ae4bc84b2f4508d70636733428613
Dan-Sutton <dewasutton@gmail.com>
2022-02-18 14:19:22 +0000

    getallusermedia function added

--- models/media.js
+++ models/media.js
@@ -1,16 +1,33 @@
 import db from "../db/connection.js";
+import dummyData from "../dummyData/data.js";
 
-export async function getAllMedia() {
-  const result = await db.query(`SELECT * FROM media;`);
-  return result.rows;
+export function getAllUserMedia(userId) {
+   const userData = dummyData.filter((item) => {
+      if (item.userId === userId) {
+         return item;
+      }
+   });
+   console.log(userData);
+   return userData;
 }
 
-export async function addMedia(aws_key, media_title, media_desc, date, location) {
-  const result = await db.query(
-    `INSERT INTO media (aws_key, media_title, media_desc, date, location) VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
-    [aws_key, media_title, media_desc, date, location]
-  );
-  return result.rows[0];
-}
+getAllUserMedia(11);
 
+// export async function getAllMedia() {
+//    const result = await db.query(`SELECT * FROM media;`);
+//    return result.rows;
+// }
 
+// export async function addMedia(
+//    aws_key,
+//    media_title,
+//    media_desc,
+//    date,
+//    location
+// ) {
+//    const result = await db.query(
+//       `INSERT INTO media (aws_key, media_title, media_desc, date, location) VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
+//       [aws_key, media_title, media_desc, date, location]
+//    );
+//    return result.rows[0];
+// }
