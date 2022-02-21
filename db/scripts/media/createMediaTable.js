423b3ccbee67398e6118bac9a6627f4842d50094
Graham <graycanny@hotmail.com>
2022-02-21 16:06:26 +0000

    db branch

--- db/scripts/media/createMediaTable.js
+++ db/scripts/media/createMediaTable.js
@@ -1,13 +1,11 @@
-// import db from "../../connection.js";
+import db from "../../connection.js";
 
-// const mediaResponse = await db.query(
-//   `CREATE TABLE IF NOT EXISTS media (id SERIAL PRIMARY KEY, aws_key TEXT, media_title TEXT, media_desc TEXT, date INT, location TEXT);`
-// );
+const mediaResponse = await db.query(
+   `CREATE TABLE IF NOT EXISTS media (id SERIAL PRIMARY KEY, aws_key TEXT, media_title TEXT, media_desc TEXT, date INT, location TEXT);`
+);
 
-// console.log(mediaResponse);
+console.log(mediaResponse);
 
-// db.end();
-
-
+db.end();
 
 // user_id INTEGER REFERENCES users(id),
--- db/scripts/media/populateMedia.js
+++ db/scripts/media/populateMedia.js
@@ -1,16 +1,16 @@
-// import db from "../../connection.js";
+import db from "../../connection.js";
 
-// const response = await db.query(
-//   `INSERT INTO media (aws_key, media_title, media_desc, date, location) VALUES ($1, $2, $3, $4, $5);`,
-//   [
-//     "12d96c0c17a8c0a5ee1d669158d6a3f4",
-//     "SOC Character",
-//     "I had a great time making this",
-//     12022021,
-//     "Latitude: 44.42577 Longitude: -122.05814",
-//   ]
-// );
+const response = await db.query(
+   `INSERT INTO media (aws_key, media_title, media_desc, date, location) VALUES ($1, $2, $3, $4, $5);`,
+   [
+      "12d96c0c17a8c0a5ee1d669158d6a3f4",
+      "SOC Character",
+      "I had a great time making this",
+      12022021,
+      "Latitude: 44.42577 Longitude: -122.05814",
+   ]
+);
 
-// console.log(response);
+console.log(response);
 
-// db.end();
+db.end();
