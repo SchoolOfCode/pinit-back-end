463b7c7f96a475469f2235f8ac6387a19a723aa3
Graham <graycanny@hotmail.com>
2022-03-02 16:44:00 +0000

    Broke the darn thing

--- db/scripts/location/createLocationTable.js
+++ db/scripts/location/createLocationTable.js
@@ -1,6 +1,9 @@
 import db from "../../connection.js";
+
+//! WILL NEED TO BE CHANGED TO ACCOMODATE FOREIGN KEY 
+
 const response = await db.query(
-    `CREATE TABLE IF NOT EXISTS location (loc_id SERIAL PRIMARY KEY, media_id INT, country TEXT, city TEXT, postcode TEXT, street TEXT, latitude DECIMAL, longitude DECIMAL);`
+    `CREATE TABLE IF NOT EXISTS location (loc_id SERIAL PRIMARY KEY, media_id FOREIGN KEY (from media table), country TEXT, city TEXT, postcode TEXT, street TEXT, latitude DECIMAL, longitude DECIMAL);`
 )
 console.log(response);
 db.end();
