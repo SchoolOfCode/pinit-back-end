5a6df23e78e26591954100684bf15b3e68491ce4
Graham <graycanny@hotmail.com>
2022-03-02 12:04:32 +0000

    location edited

--- db/scripts/location/populateLocationTable.js
+++ db/scripts/location/populateLocationTable.js
@@ -1,7 +1,7 @@
 import db from "../../connection.js";
 
 const response = await db.query(
-   `INSERT INTO location (media_id, country, city, postcode, street, latitude, longitude) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`,
+   `INSERT INTO location (media_id, country, city, postcode, street, latitude, longitude) VALUES ($1, $2, $3, $4, $5, $6, $7);`,
    [
       2,
       "UK",
