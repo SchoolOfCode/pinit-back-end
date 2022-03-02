58c51367962d4faf39c385719de00f77c69d35c7
Graham <graycanny@hotmail.com>
2022-03-02 15:07:26 +0000

    media table changed to lat lon

--- db/scripts/media/createMediaTable.js
+++ db/scripts/media/createMediaTable.js
@@ -1,7 +1,7 @@
 import db from "../../connection.js";
 
 const mediaResponse = await db.query(
-   `CREATE TABLE IF NOT EXISTS media (id SERIAL PRIMARY KEY, aws_key TEXT, media_title TEXT, media_descr TEXT, date INT, location TEXT);`
+   `CREATE TABLE IF NOT EXISTS media (id SERIAL PRIMARY KEY, aws_key TEXT, media_title TEXT, media_descr TEXT, date INT, latitude DECIMAL, longitude DECIMAL);`
 );
 
 console.log(mediaResponse);
--- db/scripts/media/populateMedia.js
+++ db/scripts/media/populateMedia.js
@@ -1,13 +1,14 @@
 import db from "../../connection.js";
 
 const response = await db.query(
-   `INSERT INTO media (aws_key, media_title, media_descr, date, location) VALUES ($1, $2, $3, $4, $5);`,
+   `INSERT INTO media (aws_key, media_title, media_descr, date, latitude, logitude) VALUES ($1, $2, $3, $4, $5, %6);`,
    [
       "12d96c0c17a8c0a5ee1d669158d6a3f4",
       "SOC Character",
       "I had a great time making this",
       12022021,
-      "Latitude: 44.42577 Longitude: -122.05814",
+      53.461948111776245,
+      -2.2922706796387105
    ]
 );
 
