b217a6fc8bc6238037baad462a0182ce6c15ede4
Graham <graycanny@hotmail.com>
2022-03-04 09:26:41 +0000

    Editing api routes

--- models/media-sql.js
+++ models/media-sql.js
@@ -1,72 +1,83 @@
-import db from "../db/connection.js";
+import db from '../db/connection.js'
 
 //Getting all media items
 export async function getAllMedia() {
-   const result = await db.query(`SELECT * FROM media;`);
-   return result.rows;
+  const result = await db.query(`SELECT * FROM media;`)
+  return result.rows
 }
 
 //Getting a media item by its ID
-export async function getMediaById(id) {
-   //finds first media item with an id that matches the parameter
-   const result = await db.query(`SELECT * FROM media WHERE id = $1;`, [id]);
-   return result.rows;
+export async function getMediaById(media_id) {
+  //finds first media item with an id that matches the parameter
+  const result = await db.query(`SELECT * FROM media WHERE media_id = $1;`, [
+    id
+  ])
+  return result.rows
 }
 
 //Getting a media item by its date
 export async function getMediaByDate(date) {
-   const result = await db.query(`SELECT * FROM media WHERE date = $1;`, [
-      date,
-   ]);
-   return result.rows;
+  const result = await db.query(`SELECT * FROM media WHERE date = $1;`, [date])
+  return result.rows
 }
 
 //Adding media
 export async function addMedia(
-   { aws_key, media_title, media_descr, date, location } // TODO: will be a separate table later on
+  { aws_key, media_title, media_descr, date, location } // TODO: will be a separate table later on
 ) {
-   const result = await db.query(
-      `INSERT INTO media (aws_key, media_title, media_descr, date, location) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
-      [aws_key, media_title, media_descr, date, location]
-   );
-   //return result.rows[result.rows.length - 1]; //TODO: check if this only returns one row or all
-   return result;
+  const result = await db.query(
+    `INSERT INTO media (aws_key, media_title, media_descr, date, location) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+    [aws_key, media_title, media_descr, date, location]
+  )
+  //return result.rows[result.rows.length - 1]; //TODO: check if this only returns one row or all
+  return result
 }
 
 //Editing a media item
 export async function editMediaByID(
-   id,
-   { aws_key, media_title, media_descr, date, location }
+  id,
+  { aws_key, media_title, media_descr, date, location }
 ) {
-   const result = await db.query(
-      `UPDATE media SET aws_key = $2, media_title = $3 , media_descr = $4, date =$5, location = $6 WHERE id = $1 RETURNING *;`,
-      [id, aws_key, media_title, media_descr, date, location]
-   );
-   return result;
+  const result = await db.query(
+    `UPDATE media SET aws_key = $2, media_title = $3 , media_descr = $4, date =$5, location = $6 WHERE id = $1 RETURNING *;`,
+    [id, aws_key, media_title, media_descr, date, location]
+  )
+  return result
 }
 
 //Deleting a media item by id
 export async function deleteMediaById(id) {
-   const result = await db.query(`DELETE FROM media WHERE id = $1 RETURNING *;`, [id])
-   return result;
+  const result = await db.query(
+    `DELETE FROM media WHERE id = $1 RETURNING *;`,
+    [id]
+  )
+  return result
 }
 
 //Deleting a media item by date
 export async function deleteMediaByDate(date) {
-   const result = await db.query(`DELETE FROM media WHERE date = $1 RETURNING *;`, [date])
-   return result;
-   
+  const result = await db.query(
+    `DELETE FROM media WHERE date = $1 RETURNING *;`,
+    [date]
+  )
+  return result
 }
 
 // Get media by location
 //! NOT SURE IF WORKS
 export async function getMediaByLocation(id, media_id) {
-   const result = await db.query(`SELECT * FROM media INNER JOIN location ON media.id = location.media_id RETURNING * WHERE $1 = $2;`, [id, media_id] )
-   return result;
+  const result = await db.query(
+    `SELECT * FROM media INNER JOIN location ON media.id = location.media_id RETURNING * WHERE $1 = $2;`,
+    [id, media_id]
+  )
+  return result
 }
 
-//Deleting a media item by location 
+//Deleting a media item by location
 export async function deleteMediaByLocation(id, media_id) {
-   const result = await db.query(`DELETE FROM media INNER JOIN location ON media.id = location.media_id WHERE $1 = $2;`, [id, media_id])
-   return result;
+  const result = await db.query(
+    `DELETE FROM media INNER JOIN location ON media.id = location.media_id WHERE $1 = $2;`,
+    [id, media_id]
+  )
+  return result
 }
--- routes/users.js
+++ routes/users.js
@@ -1,48 +1,44 @@
-import express from "express";
-import { getDataByEmail, getAllUsers } from "../models/users-sql.js";
+import express from 'express'
 
-const router = express.Router();
+import { getDataByEmail, getAllUsers } from '../models/users-sql.js'
+
+const router = express.Router()
 
 // GET all users
-router.get("/users", async function (req, res) {
-    const data = await getAllUsers();
-    if (data) {
-       return res.json({
-          success: true,
-          message: "Fetched all users",
-          payload: data,
-       });
-    } else {
-       return res.json({
-          success: false,
-          message: "something went wrong",
-       });
-    }
- });
-
-
+router.get('/users', async function (req, res) {
+  const data = await getAllUsers()
+  if (data) {
+    return res.json({
+      success: true,
+      message: 'Fetched all users',
+      payload: data
+    })
+  } else {
+    return res.json({
+      success: false,
+      message: 'something went wrong'
+    })
+  }
+})
 
 /* GET users listing by email, create USER if not there */
-router.get("/users/:email", async function (req, res) {
-    console.log('in email function')
-    const email = req.params;
-    console.log(email)
-    const data = await getDataByEmail(email);
-    if(data){
+router.get('/users/:email', async function (req, res) {
+  console.log('in email function')
+  const email = req.params
+  console.log(email)
+  const data = await getDataByEmail(email)
+  if (data) {
     res.json({
-    success: true,
-    payload: data,
-    });
-    } else { return res.json({
-    success: false,
-    message: "Barry isn't here"
-})}
-});
-
-
-
-
-
+      success: true,
+      payload: data
+    })
+  } else {
+    return res.json({
+      success: false,
+      message: "Barry isn't here"
+    })
+  }
+})
 
 // /* PUT user */
 // router.post("/users", async function (req, res) {
