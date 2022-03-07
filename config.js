8a09e8e4a581c203a7c7515daf92c74091c7d1b1
Graham <graycanny@hotmail.com>
2022-03-07 10:38:19 +0000

    port added back in

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const db = {
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
-  //   port: process.env.DB_PORT,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD
 }
 
