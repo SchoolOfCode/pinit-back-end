ef8f377df198a467ca5abb2b7821a7fda3e21406
Graham <graycanny@hotmail.com>
2022-03-13 18:42:13 +0000

    added some pics to lake district

--- db/scripts/master/populateAllTables.js
+++ db/scripts/master/populateAllTables.js
@@ -1,24 +1,26 @@
 import db from '../../connection.js'
 
-const response = await db.query(`INSERT INTO users(username, email)
-VALUES ('barryshitpeas', 'barry.shitpeas@hotmail.com'),
-       ('dannyleafblower', 'danny.leafblower@aol.com'),
-       ('mistymoo', 'misty.moo@woofwoof.com');
-
-INSERT INTO place(user_id, lat, lng)
-VALUES (1, 51.545585399817966, -0.5494009347372437),
+const response = await db.query(
+  `INSERT INTO users(username, email)
+    VALUES ('harry', 'harry.walker@hotmail.com'),
+    ('dannyleafblower', 'danny.leafblower@aol.com'),
+    ('mistymoo', 'misty.moo@woofwoof.com');
+       
+    INSERT INTO place(user_id, lat, lng)
+VALUES (1, 54.436951570145, -2.969584848090621),
        (1, 51.396331784441024, -0.037297327028535915),
-       (2, 51.646968059980836, -0.3559174363750419),
-       (3, 51.47273644130273, 0.14002693637736469),
-       (4, 53.33586381370047, -2.3462119895986087);
+       (1, 51.646968059980836, -0.3559174363750419),
+       (1, 51.47273644130273, 0.14002693637736469),
+       (1, 53.33586381370047, -2.3462119895986087);
 
 INSERT INTO media(loc_id, img_url, title, place, notes)
-VALUES (1, 'https://media.timeout.com/images/105186767/750/422/image.jpg', 'Barrys Telly', 'London', 'A pic of Barrys Telly'),
-       (1, 'https://media.timeout.com/images/105237777/image.jpg', 'Barry in Germany', 'Leafton', 'Big barry'),
-       (2, 'https://www.fodors.com/wp-content/uploads/2018/10/HERO_UltimateRome_Hero_shutterstock789412159.jpg', 'Blowing leaves', 'Back Garden', 'In the park'),
-       (3, 'https://www.thetrainline.com/cms/media/1701/torre_pisa_pisa_tower.jpg?mode=crop&width=1080&height=1080&quality=70', 'Staring at cats', 'Back yard', 'Through the fence'),
-       (4, 'https://media.timeout.com/images/105489544/image.jpg', 'Sniffed a bush', 'The Park', 'Good Bush'),
-       (4, 'https://www.fodors.com/wp-content/uploads/2019/11/HERO_Venice__FloatingCityBuilt_iStock-986940360.jpg', 'Sniffed another bush', 'Hullard Park', 'Another really good bush');`)
+VALUES (1, 'https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2208&q=80', 'Camping with friends', 'Lake District', 'Sat around the campfire on the first night, good thing Barry remembered his acoustic guitar!'),
+       (1, 'https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80', 'Tent Village', 'Campsite', 'It felt like our own little village!'),
+       (1, 'https://images.unsplash.com/uploads/141148589884100082977/a816dbd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80', 'First day hike', 'Langdale', 'On the first day we took a long hike through Langdale'),
+       (1, 'https://images.unsplash.com/photo-1588099631365-aacc6480737b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1775&q=80', 'Drinks after', 'Old Dungeon Ghyll', 'Ended up in a lovely pub for a well deserved drink at the end of the day'),
+       (1, 'https://images.unsplash.com/photo-1612727002301-bafce2446514?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80', 'On the way back', 'Langdale night', 'Walking back to the tents, it was such a lovely night to be outside.'),
+       (4, 'https://www.fodors.com/wp-content/uploads/2019/11/HERO_Venice__FloatingCityBuilt_iStock-986940360.jpg', 'Sniffed another bush', 'Hullard Park', 'Another really good bush');`
+)
 
 console.log(response)
 
